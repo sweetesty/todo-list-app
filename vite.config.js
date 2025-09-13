4725ba3972e87d41fe7e28aa2a26f73cc0743d90
Esther <estherolukorede12@gmail.com>
2025-09-13 20:07:28 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/todo-list-app/',
 
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || '/todo-list-app' 
 })
